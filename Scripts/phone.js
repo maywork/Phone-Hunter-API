0a2982c41e71d5e43b5916e6d62a303672e08e80
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2023-09-01 17:28:27 +0600

    added data load, search and refresh content with search functionalities

--- Scripts/phone.js
+++ Scripts/phone.js
@@ -1,1 +1,51 @@
-console.log('works!');
+// previous fetch alternative => works same
+
+const loadPhone = async (searchText) => {
+  const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
+  const data = await response.json();
+  const phones = data.data;
+  // console.log(phones);
+  displayPhones(phones);
+};
+
+const displayPhones = phones => {
+  // console.log(phones[0].brand);
+
+  // 1. get the container
+  const phonesContainer = document.getElementById('phones-container');
+  // clear container before append
+  phonesContainer.textContent = '';
+
+  phones.forEach(phone => {
+    console.log(phone);
+    // 2. create a div
+    const phoneCard = document.createElement('div');
+    phoneCard.classList = `card bg-gray-100 p-4 shadow-xl`;
+    // 3. set innerHTML
+    phoneCard.innerHTML = `
+    <figure>
+    <img src="${phone.image}" alt="phones" />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">${phone.phone_name}</h2>
+      <p>${phone.slug}</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+      </div>
+    </div>
+    `;
+    // 4 append to parent
+    phonesContainer.appendChild(phoneCard);
+  });
+}
+
+
+// handle search button
+const handleSearch = () => {
+  const searchField = document.getElementById('search-field');
+  const searchText = searchField.value;
+  console.log(searchText);
+  loadPhone(searchText);
+}
+
+// loadPhone();
